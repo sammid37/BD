984c5564584dbd0189d9f06999b6fd71fb7d669f
sammid37 <samanthadmedeiros37@gmail.com>
2023-11-25 15:55:55 -0300

    ⚙ DB config

--- src/main/java/com/voleyrant/crud/util/ConnectionConfig.java
+++ src/main/java/com/voleyrant/crud/util/ConnectionConfig.java
@@ -1,6 +1,6 @@
 package src.main.java.com.voleyrant.crud.util;
 
-public class ConfigDB {
+public class ConnectionConfig {
   public static final String URL = "jdbc:mysql://localhost:3306/projetoBD";
   public static final String USUARIO = "root";
   public static final String SENHA = "";
